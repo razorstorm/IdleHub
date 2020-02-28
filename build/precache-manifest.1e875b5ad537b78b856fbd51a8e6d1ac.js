09cbbed1027c5d518aa0f9083da662124f9c41a8
Jason Jiang <jjiang1991@gmail.com>
2020-02-27 22:56:41 -0800

    favicon and stuff

---
+++ build/precache-manifest.1e875b5ad537b78b856fbd51a8e6d1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a06868ac7f6d7a85418fe01cfd248c",
+    "url": "/IdleHub/index.html"
+  },
+  {
+    "revision": "f379fabb8cda9a6e2a57",
+    "url": "/IdleHub/static/css/main.8bf09517.chunk.css"
+  },
+  {
+    "revision": "5e4f748d4da08c0f435b",
+    "url": "/IdleHub/static/js/2.ac7e234d.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/IdleHub/static/js/2.ac7e234d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f379fabb8cda9a6e2a57",
+    "url": "/IdleHub/static/js/main.dc953239.chunk.js"
+  },
+  {
+    "revision": "cddddb19a7c36f197e04",
+    "url": "/IdleHub/static/js/runtime-main.7e3f7022.js"
+  }
+]);
